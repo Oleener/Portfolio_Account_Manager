c124af58d199a5abe1ae535e4ef10dbd88fbba96
Kirill Panov <kirpa8604@gmail.com>
2022-03-26 09:46:39 -0700

    Added generate_asset_mode_string function to generate Asset Mode info for the asset in portfolio

--- utils/formatters.py
+++ utils/formatters.py
@@ -1,6 +1,6 @@
 def format_performance_percentage(number):
   if float(number) > 0:
-    number *= 100     
+    #number *= 100     
     formatted_number = f"+{number:.2f}%"
   elif float(number) == 0:
     formatted_number = f"{number:.2f}%"
@@ -31,3 +31,23 @@ def generate_detailed_asset_string(asset_code, asset_holdings, asset_avg_buy_pri
   asset_performance_percentage = format_performance_percentage(asset_total_profit_loss_percentage)
   asset_performance_currency = format_performance_currency(asset_total_profit_loss_currency)
   return f"{asset_code} - {asset_holdings} {asset_code}; Price: ${current_price}; Balance: ${asset_balance:.2f}; Avg Buy Price: ${asset_avg_buy_price:.2f}; Total Profit/Loss: {asset_performance_percentage}({asset_performance_currency})"
+
+#Function to gather asset info and generate a string to show details on asset in Asset Mode
+def generate_asset_mode_string(asset, portfolio):
+  portfolio_performance_percentage = format_performance_percentage(asset['asset_total_profit_loss_percentage'])
+  porfolio_performance_currency = format_performance_currency(asset['asset_total_profit_loss_currency'])
+  asset_info_str = f"""
+Asset Management Mode
+-------------------------
+Managing asset: {asset['asset_code']}
+Portfolio: {portfolio['portfolio_name']}
+----------------------------
+Asset holdings: {asset['asset_holdings']}
+Asset average buy price: ${asset['asset_avg_buy_price']}
+Asset investment: ${asset['sum_of_investments']}
+Asset current price: ${asset['current_price']}
+Asset current balance: ${asset['asset_balance']}
+Asset total profit/loss: {porfolio_performance_currency}({portfolio_performance_percentage})
+----------------------------
+"""
+  return asset_info_str
